4308f7d966c617423e6e194a449d51083d9d0334
tmeissner <programming@goodcleanfun.de>
2020-05-28 20:02:31 +0200

    Add simulation of the PSL examples, fixes #1

---
+++ sim/template.vhd
@@ -0,0 +1,24 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+
+
+entity tb___DUT__ is
+end entity tb___DUT__;
+
+
+architecture sim of tb___DUT__ is
+
+  signal clk   : std_logic := '1';
+  signal cycle : natural   := 0;
+
+begin
+
+
+  clk <= not clk after 1 ns;
+
+  cycle <= cycle + 1 when rising_edge(clk);
+
+  DUT : entity work.__DUT__(psl) port map (clk);
+
+
+end architecture sim;
---
+++ sim/tests.mk
@@ -0,0 +1,25 @@
+psl_tests := \
+psl_always \
+psl_logical_implication \
+psl_never \
+psl_next \
+psl_next_3 \
+psl_next_a \
+psl_next_e \
+psl_next_event \
+psl_next_event_4 \
+psl_next_event_e \
+psl_next_event_a \
+psl_until \
+psl_before \
+psl_eventually \
+psl_sere \
+psl_sere_overlapping_suffix_impl \
+psl_sere_non_overlapping_suffix_impl \
+psl_sere_consecutive_repetition \
+psl_sere_non_consecutive_repeat_repetition \
+psl_sere_non_consecutive_goto_repetition \
+psl_cover \
+psl_sere_within \
+psl_sere_or \
+psl_sere_len_matching_and
