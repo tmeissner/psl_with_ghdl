b018d2e608a2c911686f02baf52748cd0413a3b8
tmeissner <programming@goodcleanfun.de>
2020-06-03 00:02:30 +0200

    Add examples for currently unsupported rose(), fell() & stable() PSL functions

---
+++ src/psl_fell.vhd
@@ -0,0 +1,57 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+
+use work.pkg.all;
+
+
+entity psl_fell is
+  port (
+    clk : in std_logic
+  );
+end entity psl_fell;
+
+
+architecture psl of psl_fell is
+
+  signal a, b : std_logic;
+
+begin
+
+
+  --                              01234567890
+  SEQ_A : sequencer generic map ("_-__-_---__") port map (clk, a);
+  SEQ_B : sequencer generic map ("__-__-___-_") port map (clk, b);
+
+
+  -- All is sensitive to rising edge of clk
+  default clock is rising_edge(clk);
+
+  -- This assertion holds
+  FELL_0_a : assert always (fell(a) -> b);
+
+  -- Workaround needed before fell() is implemented
+  -- With VHDL glue logic generating the
+  -- previous value of a and simple comparing the two values
+  d_reg : block is
+    signal a_prev : std_logic := '0';
+  begin
+    process (clk) is
+    begin
+      if rising_edge(clk) then
+        a_prev <= a;
+      end if;
+    end process;
+    FELL_1_a : assert always (not a and a_prev -> b);
+  end block d_reg;
+
+  -- Another workaround by using simple SERE concatenation on LHS
+  FELL_2_a : assert always {a; not a} |-> b;
+
+  -- Stop simulation after longest running sequencer is finished
+  -- Simulation only code by using pragmas
+  -- synthesis translate_off
+  stop_sim(clk, 11);
+  -- synthesis translate_on
+
+
+end architecture psl;
---
+++ src/psl_stable.vhd
@@ -0,0 +1,64 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+
+use work.pkg.all;
+
+
+entity psl_stable is
+  port (
+    clk : in std_logic
+  );
+end entity psl_stable;
+
+
+architecture psl of psl_stable is
+
+  signal valid, ack, a : std_logic;
+  signal b             : std_logic_vector(3 downto 0);
+
+begin
+
+
+  --                                  0123456789
+  SEQ_VALID : sequencer generic map ("_--__---__") port map (clk, valid);
+  SEQ_ACK   : sequencer generic map ("__-____-__") port map (clk, ack);
+  SEQ_A     : sequencer generic map ("_--_______") port map (clk, a);
+  SEQ_B : hex_sequencer generic map ("0110066600") port map (clk, b);
+
+
+  -- All is sensitive to rising edge of clk
+  default clock is rising_edge(clk);
+
+  -- This assertion holds
+  STABLE_0_a : assert always {not valid; valid} -> next (stable(a) until_ ack);
+
+  -- This assertion holds
+  STABLE_1_a : assert always {not valid; valid} -> next (stable(b) until_ ack);
+
+  -- Workaround needed before stable() is implemented
+  -- With VHDL glue logic generating the
+  -- previous value of a and simple comparing the two values
+  a_reg : block is
+    signal a_prev : std_logic;
+    signal b_prev : std_logic_vector(b'range);
+  begin
+    process (clk) is
+    begin
+      if rising_edge(clk) then
+        a_prev <= a;
+        b_prev <= b;
+      end if;
+    end process;
+    STABLE_2_a : assert always {not valid; valid} |=> (a = a_prev until_ ack);
+    STABLE_3_a : assert always {not valid; valid} |=> (b = b_prev until_ ack);
+  end block a_reg;
+
+
+  -- Stop simulation after longest running sequencer is finished
+  -- Simulation only code by using pragmas
+  -- synthesis translate_off
+  stop_sim(clk, 10);
+  -- synthesis translate_on
+
+
+end architecture psl;
