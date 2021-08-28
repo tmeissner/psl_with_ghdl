5465e5e2cf27a4e77ac8aee28445f78f6a08d76b
tmeissner <programming@goodcleanfun.de>
2021-08-28 15:18:43 +0200

    Add example using generate indexes in PSL properties to vunit example, see ghdl/ghdl#1850

--- src/psl_vunit.vhd
+++ src/psl_vunit.vhd
@@ -1,5 +1,6 @@
 library ieee;
   use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 use work.pkg.all;
 
@@ -17,6 +18,7 @@ end entity psl_vunit;
 architecture beh of psl_vunit is
 
   signal a, b : std_logic;
+  signal c : std_logic_vector(3 downto 0);
 
 begin
 
@@ -24,12 +26,14 @@ begin
   --                              012345
   SEQ_A : sequencer generic map ("--____") port map (clk, a);
   SEQ_B : sequencer generic map ("_-____") port map (clk, b);
+  --
+  SEQ_C : hex_sequencer generic map ("0123456789ABCDEF") port map (clk, c);
 
 
   -- Stop simulation after longest running sequencer is finished
   -- Simulation only code by using pragmas
   -- synthesis translate_off
-  stop_sim(clk, 6);
+  stop_sim(clk, 18);
   -- synthesis translate_on
 
 
